06ac153e5d41401d2e803fadd6fced68a25e9624
Víctor <Vctor@DESKTOP-TI96FII>
2017-06-06 22:57:07 +0200

    Se implementa diferenciación entre expertos y novatos, antes no diferenciada.

--- 2017PCTR_2C04/src/pl02/Simulador.java
+++ 2017PCTR_2C04/src/pl02/Simulador.java
@@ -8,14 +8,14 @@ public class Simulador {
 	public static int NUM_VUELTAS=5;
 	public static void main(String[] args) {
 		
-		
+		int limite=NUM_EXPERTOS+NUM_NOVATOS;
+		boolean bool=true;
 		Club clubA=new Club(NUM_PELOTAS,NUM_PALOS);
 		///ExecutorService exec=Executors.newSingleThreadExecutor();
-		for (int i = 0; i < 14; i++) {
-			//Thread jugador= new Thread(new Jugador(i,true,NUM_VUELTAS,clubA));
-			//exec.execute(new  Jugador(i,true,NUM_VUELTAS,clubA));
-			//jugador.start();
-			new Jugador(i,true,NUM_VUELTAS,clubA).start();
+		for (int i = 1; i <= limite; i++) {
+			if(i>NUM_EXPERTOS)
+				bool=false;
+			new Jugador(i,bool,NUM_VUELTAS,clubA).start();
 		}
 	}
 }
