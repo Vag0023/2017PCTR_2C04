321c2ced9151da87b6f2da07f9049c9685cf9d37
Víctor <Vctor@DESKTOP-TI96FII>
2017-06-07 10:58:31 +0200

    Se añaden pruebas de los asserts de Club y eliminan printlns innecesarios.

--- 2017PCTR_2C04/src/pl02/Jugador.java
+++ 2017PCTR_2C04/src/pl02/Jugador.java
@@ -3,51 +3,58 @@ package pl02;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
-public class Jugador extends Thread implements Runnable{
+public class Jugador extends Thread implements Runnable {
 	private int id;
 	private int numVueltas;
 	private boolean experto;
 	private Club elClub;
 	private int palos;
 	private int pelotas;
-	public Jugador(int id, boolean experto, int numVueltas,  Club elClub ){
-		this.id=id;
-		this.experto=experto;
-		this.numVueltas=numVueltas;
-		this.elClub=elClub;
+
+	public Jugador(int id, boolean experto, int numVueltas, Club elClub) {
+		this.id = id;
+		this.experto = experto;
+		this.numVueltas = numVueltas;
+		this.elClub = elClub;
 	}
-	public void run(){
-		//int a=0;
-		for (int i=0;i<numVueltas;i++){
-			if (experto){
-				palos=ThreadLocalRandom.current().nextInt(2, 5 + 1);
-				pelotas=1;
-			}else{
-				palos=2;
-				pelotas=ThreadLocalRandom.current().nextInt(1, 5 + 1);
+
+	public void run() {
+
+		for (int i = 0; i < numVueltas; i++) {
+			if (experto) {
+				palos = ThreadLocalRandom.current().nextInt(2, 5 + 1);
+				pelotas = 1;
+			} else {
+				palos = 2;
+				pelotas = ThreadLocalRandom.current().nextInt(1, 5 + 1);
 			}
-			
-			System.out.println(id+"["+palos+","+pelotas+"] reserva ");
+
+			System.out.println(id + "[" + palos + "," + pelotas + "] reserva ");
 			elClub.reservar(pelotas, palos);
-			System.out.println(id+"["+palos+","+pelotas+"] jugar ");
+
+			System.out.println(id + "[" + palos + "," + pelotas + "] jugar ");
 			try {
 				TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextInt(500, 1000 + 1));
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
 			}
-			System.out.println(id+"["+palos+","+pelotas+"] devolver ");
+			System.out.println(id + "[" + palos + "," + pelotas + "] devolver ");
 			elClub.devolver(pelotas, palos);
-			System.out.println(id+"["+palos+","+pelotas+"] descansar ");
-			
+
+			System.out.println(id + "[" + palos + "," + pelotas + "] descansar ");
+
 			try {
 				TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextInt(500, 1000 + 1));
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
+
 				e.printStackTrace();
 			}
-			//a=i;
+
+			// Prueba asserts Club
+			// elClub.reservar(250, 250);
+			// elClub.devolver(500, 500);
 		}
-		//System.out.println(id+ "      "+a);
+
 	}
 }
